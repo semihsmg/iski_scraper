9b291697f1873b02ee31d51f92db7d74089214ee
Semih <23503571+semihsmg@users.noreply.github.com>
2018-10-13 15:19:56 +0300

    iski_scraper.py güncelleme
    - Programın çalışırken kapladığı yer azaltıdı.
    - Mahalle kontrolu düzeltildi

--- su_bildirim.py
+++ su_bildirim.py
@@ -3,17 +3,20 @@ import requests
 import time
 import send_mail
 
-# seconds = 30.0
 seconds = 3600.0
 start_time = time.time()
 
+page_link = 'https://www.iski.istanbul/web/tr-TR/ariza-kesinti'
+
 semt = 'Fatih'
-mah = 'Cerrahpaşa'
+mah = 'Aksaray'
 is_there_semt = False
 is_there_mah = False
 
 first_line = ''
 second_line = ''
+
+excluded_texts = ['', ':', 'Açıklama', 'İlçe', 'Etkilenen Mahalleler', 'Başlama Tarihi', 'Bitiş Tarihi']
 
 
 def indexes(lst, element):
@@ -27,27 +30,27 @@ def indexes(lst, element):
         result.append(offset)
 
 
-def report():
+def time_stamp():
     print(time.strftime('%d %b %Y - %H:%M:%S'))
 
 
 while True:
     clean_page_txt = []
 
-    page_link = 'https://www.iski.istanbul/web/tr-TR/ariza-kesinti'
     # fetch the content from url
     page_response = requests.get(page_link, timeout=5)
     # parse html
     page_content = BeautifulSoup(page_response.text, 'html.parser')
 
-    for lnk in page_content.find_all('a'):
-        lnk.decompose()
-
     targeted_class = page_content.find(class_='col-xs-12 col-sm-12 col-md-9 col-lg-9 sp-rightcol')
     targeted_class_items = targeted_class.find_all('td')
 
     for line in targeted_class_items:
-        clean_page_txt.append(line.get_text().strip())
+        txt = line.get_text().strip()
+        if txt in excluded_texts:
+            continue
+        else:
+            clean_page_txt.append(txt.translate(str.maketrans({',': ' '})))
 
     for value in clean_page_txt:
         if (semt and mah) in value:
@@ -58,15 +61,16 @@ while True:
     if is_there_semt and is_there_mah:
         index_list = indexes(clean_page_txt, semt)
         for i in index_list:
-            extracted_info = clean_page_txt[i + 12][0:11] + clean_page_txt[i + 12][26:]
-            report()
-            print(semt + ', ' + mah + ' Mah: ')
-            print(extracted_info)
-            send_mail.send(str(semt + ', ' + mah + ' Mah: ' + extracted_info))
+            if mah in clean_page_txt[i + 1]:
+                extracted_info = clean_page_txt[i + 4][0:11] + clean_page_txt[i + 4][26:]
+                time_stamp()
+                print(semt + ', ' + mah + ' Mah: ')
+                print(extracted_info)
+                # send_mail.send(str(semt + ', ' + mah + ' Mah: ' + extracted_info))
 
     else:
-        report()
-        print(semt + ', ' + mah + ' bölgesine ait veri bulunmamakta!')
+        time_stamp()
+        print(semt + ', ' + mah + ' bolgesine ait veri bulunmamakta!')
 
     time.sleep(seconds - ((time.time() - start_time) % seconds))  # seconds later run the program again
     print()
